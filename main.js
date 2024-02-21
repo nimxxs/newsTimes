273c0e4680aa28228ed2fd39b5f14359c4beab8f
bigdata <tnals2199@naver.com>
2024-02-21 21:47:31 +0900

    HTML 및 CSS 및 기사 불러오기 및 반응형 추가

--- main.js
+++ main.js
@@ -1,13 +1,64 @@
-const API_KEY = `b990a62c995341538b0f3456791a1bfc`
-let news = []
+const API_KEY = `b990a62c995341538b0f3456791a1bfc`;
+let newsList = [];
+let searchButton = document.getElementById("searchButton");
+let input = document.getElementById("input");
+let parent = document.querySelector("parent");
 
+// api 가져오기
 const getLatesNews = async () => {
     const url = new URL (
         `https://mynewtimes.netlify.app//top-headlines?country=kr&apiKey=${API_KEY}`
     )
-    const response = await fetch(url)
-    const data = await response.json()
-    news = data.articles
-    console.log("ddd", news)
+    const response = await fetch(url);
+    const data = await response.json();
+    newsList = data.articles;
+    render();
+    console.log("ddd", newsList);
+}
+
+// 이미지 유효성 검사
+const imageError = (imageUrl) => {
+    // 새로운 'Image' 객체를 생성 -> js에서 이미지를 다룰 때 사용되는 내장 객체, 이미지를 동적 생성함.
+    const image = new Image();
+    image.src = imageUrl
+    if(!image.complete) {
+        return false
+    } else {
+        return true
+    }
+}
+
+const render = () => {
+    const newsHTML = newsList.map(
+        (news) => 
+        `<div class="row news">
+            <div class="col-lg-4">
+                <img class="newsImgSize" src="${news.urlToImage && imageError(news.urlToImage) ? news.urlToImage : 'https://t4.ftcdn.net/jpg/04/99/93/31/360_F_499933117_ZAUBfv3P1HEOsZDrnkbNCt4jc3AodArl.jpg'}">
+            </div>
+            <div class="col-lg-8">
+                <h2>${news.title}</h2>
+                <p>${news.description ? news.description.slice(0,200) + '...' : '내용없음'}</p>
+                <div>
+                    ${news.source.name ? news.source.name : 'no source'} ${moment(news.publishedAt).fromNow()}
+                </div>
+            </div>
+        </div>`).join('');
+        console.log("html", newsHTML)
+
+    document.getElementById("newsBoard").innerHTML = newsHTML;
 }
 getLatesNews()
+
+// 검색창
+searchButton.addEventListener("click", () => {
+    input.classList.toggle("active");
+    input.focus();
+})
+
+// 햄버거바
+function openNav() {
+    document.getElementById("mySideNav").style.width = "250px";
+}
+function closeNav() {
+    document.getElementById("mySideNav").style.width = "0";
+}
