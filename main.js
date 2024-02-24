c3a3faf1ccf43394f17f8de82b0b1210d3b298e4
bigdata <tnals2199@naver.com>
2024-02-25 01:03:03 +0900

    pagination 추가

--- main.js
+++ main.js
@@ -6,30 +6,41 @@ let parent = document.querySelector("parent");
 const menus = document.querySelectorAll(".menus button");
 menus.forEach(menu => menu.addEventListener("click", (event) => getNewsByCategory(event)))
 let url = new URL(
-    `https://mynewtimes.netlify.app//top-headlines?country=kr&apiKey=${API_KEY}`
-    // `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
+    // `https://mynewtimes.netlify.app//top-headlines?country=kr&apiKey=${API_KEY}`
+    `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
     )
+let totalResults = 0;
+let page = 1;
+const pageSize = 10;
+const groupSize = 5;
 
 // api 가져오기
 const getLatesNews = async () => {
     url = new URL (
-        `https://mynewtimes.netlify.app//top-headlines?country=kr&apiKey=${API_KEY}`
-        // `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
-    )
-    getNews();
-}
-
+        // `https://mynewtimes.netlify.app//top-headlines?country=kr&apiKey=${API_KEY}`
+        `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
+        )
+        getNews();
+    }
+    
 // 기사 가져오는 함수
 const getNews = async () => {
     try {
+        url.searchParams.set("page",page);
+        url.searchParams.set("pageSize",pageSize);
+
         const response = await fetch(url);
         const data = await response.json();
+        console.log("ddd", data)
+
         if (response.status === 200) {
             if (data.articles.length === 0) {
                 throw new Error("No result for this search");
             }
             newsList = data.articles;
+            totalResults = data.totalResults;
             render();
+            pagiNationRender();
         } else {
             throw new Error(data.message);
         }
@@ -43,8 +54,8 @@ const getNews = async () => {
 const getNewsByCategory = async (event) => {
     const category = event.target.textContent.toLowerCase();
     url = new URL(
-        `https://mynewtimes.netlify.app//top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`
-        // `https://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`
+        // `https://mynewtimes.netlify.app//top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`
+        `https://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`
     );
     getNews();
 }
@@ -54,8 +65,8 @@ const getNewsByKeyword = async () => {
     const keyword = document.getElementById("input").value;
     console.log("keyword", keyword);
     url = new URL(
-        `https://mynewtimes.netlify.app//top-headlines?q=${keyword}&apiKey=${API_KEY}`
-        // `https://newsapi.org/v2/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`
+        // `https://mynewtimes.netlify.app//top-headlines?q=${keyword}&apiKey=${API_KEY}`
+        `https://newsapi.org/v2/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`
         );
     getNews();
 }
@@ -91,7 +102,6 @@ const render = () => {
 
     document.getElementById("newsBoard").innerHTML = newsHTML;
 }
-getLatesNews()
 
 // 에러 메시지
 const errorRender = (errorMessage)  => {
@@ -115,3 +125,66 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySideNav").style.width = "0";
 }
+
+
+// 페이지네이션
+const pagiNationRender = () => {
+    // totalPages
+    const totalPages = Math.ceil(totalResults / pageSize);
+    // pageGroup
+    const pageGroup = Math.ceil(page / groupSize);
+    // lastPage
+    let lastPage = pageGroup * groupSize;
+    if(lastPage > totalPages) {
+        lastPage = totalPages;
+    }
+    // firstPage
+    const firstPage = lastPage - (groupSize - 1) <= 0 ? 1 : lastPage - (groupSize - 1);
+
+    let paginationHTML = ``
+
+    // 이전 버튼 추가
+    paginationHTML += `<li class="page-item preToPage">
+        <a class="page-link" aria-label="Previous">
+            <span aria-hidden="true">«</span>
+        </a>
+    </li>`;
+    for (let i = firstPage; i <= lastPage; i++) {
+        paginationHTML +=
+            `<li class="page-item ${i === page ? "active" : ""}" onclick="moveToPage(${i})">
+                <a class="page-link">${i}</a>
+            </li>`;
+    }
+    // 다음 버튼 추가
+    paginationHTML += `<li class="page-item nextToPage">
+        <a class="page-link" aria-label="Next">
+            <span aria-hidden="true">»</span>
+        </a>
+    </li>`;
+    document.querySelector(".pagination2").innerHTML = paginationHTML;
+
+    document.querySelector(".preToPage").addEventListener("click", preToPage);
+    document.querySelector(".nextToPage").addEventListener("click", nextToPage);
+
+}
+// 1페이지, 2페이지.. 한 페이지씩 넘기기
+const moveToPage = (pageNum) => {
+    console.log("moveToPage",pageNum);
+    page = pageNum;
+    getNews();
+}
+// 이전 페이지, 다음 페이지
+const preToPage =  () => {
+    if (page > 1) {
+        page --;
+        getNews();
+    }
+}
+const nextToPage = () => {
+    const totalPages = Math.ceil(totalResults / pageSize);
+    if (page < totalPages) {
+        page ++;
+        getNews();
+    }
+}
+getLatesNews();
